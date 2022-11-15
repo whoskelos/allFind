273cfb23f062ad1ab7d55b558d9877c1459402bf
kelvin-desktop <kelvinguerrero2199@gmail.com>
2022-11-15 02:09:51 +0100

    el buscador ya funciona completamente

--- js/form-busqueda.js
+++ js/form-busqueda.js
@@ -1,7 +1,9 @@
 var listaEquipos = [];
 onload = () => {
     cargarEquipos();
-    document.getElementById("btnBuscar").addEventListener("click",filtrarEquipo);
+    document
+        .getElementById("btnBuscar")
+        .addEventListener("click", filtrarEquipo);
 };
 
 function cargarEquipos() {
@@ -14,6 +16,7 @@ function filtrarEquipo() {
     let optTipo = document.getElementById("prefEquipo").value; // si es portatil o sobremesa
     let optPrecio = document.getElementById("presupuesto").value; //rango de precio en el que se encuentra
     optPrecio = optPrecio.split("-");
+    console.log(optPrecio);
 
     //declaramos flag $errror = false;
     let error = false;
@@ -23,18 +26,71 @@ function filtrarEquipo() {
         error = true;
     }
 
+    if (document.getElementById("msgErr")) {
+        document.getElementById("msgErr").remove();
+    }
+    //si ya existe el div lo elimina
+    if (document.getElementById("mostrarEquipos")) {
+        document.getElementById("mostrarEquipos").remove();
+    }
     //si no hay errores realizamos la búsqueda
     if (!error) {
-        console.log(optActividad);
-        console.log(optTipo);
-        console.log(optPrecio);
-        let listaFiltrada = listaEquipos.filter(equipo => equipo.tipo.toLowerCase() == optActividad.toLowerCase()).filter(ele => ele.portatil.toLowerCase() == optTipo.toLowerCase());
-        console.log("EQUIPOS FILTRADOS: ",listaFiltrada);
-    }else {
+        //filtro la lista en base a lo que el cliente selecciona en los select
+        let listaFiltrada = listaEquipos
+            .filter(
+                (equipo) =>
+                    equipo.tipo.toLowerCase() == optActividad.toLowerCase()
+            )
+            .filter(
+                (ele) => ele.portatil.toLowerCase() == optTipo.toLowerCase()
+            )
+            .filter((ele) => {
+                if (
+                    (parseFloat(ele.precio) >= parseFloat(optPrecio[0]) &&
+                    parseFloat(ele.precio) <= parseFloat(optPrecio[1])) ||
+                    parseFloat(ele.precio) > parseFloat(optPrecio[0])
+                ) {
+                    return 1;
+                }
+                return 0;
+            });
+
+        //compruebo si el array esta vacio para mostrar mensaje de no se han encontrado coincidencias o pintar los equipos filtrados
+        if (listaFiltrada.length <= 0) {
+            document.querySelector("#mensajeError").innerHTML += `
+            <div id="msgErr" class="errorFiltro">No se encontraron coincidencias</div>
+            `;
+        } else {
+            //creo el div donde se van a mostrar los equipos filtrados
+            let divEquiposFiltrados = document.createElement("div");
+            divEquiposFiltrados.id = "mostrarEquipos";
+            //lo inserto en el DOM
+            document
+                .getElementById("filaEquiposFiltrados")
+                .appendChild(divEquiposFiltrados);
+            divEquiposFiltrados.classList.add(
+                "col",
+                "d-flex",
+                "flex-wrap",
+                "justify-content-around"
+            );
+            //recorro la lista y los pinto
+            listaFiltrada.forEach((equipo) => {
+                divEquiposFiltrados.innerHTML += `
+                            <div class="card mb-3" style="width: 18rem;">
+                                <img src="/img/${equipo.imagen}" class="card-img-top" alt="${equipo.modelo}">
+                                <div class="card-body">
+                                    <h5 class="card-title text-center">${equipo.modelo}</h5>
+                                    <p class="card-text"><strong>Precio: </strong>${equipo.precio}$</p>
+                                    <p class="card-text"><strong>CPU: </strong>${equipo.cpu}</p>
+                                    <p class="card-text"><strong>RAM: </strong>${equipo.ram} GB</p>
+                                    <a class="btn btn-primary" id="btn-verDetalles" onclick="verDetalleEquipo(${equipo.id_equipo})">Ver detalles</a>
+                                </div>
+                            </div>
+                        `;
+            });
+        }
+    } else {
         console.log("Seleccione todas las opciones");
     }
-
 }
-
-
-
